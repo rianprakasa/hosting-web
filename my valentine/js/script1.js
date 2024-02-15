689b48c992196348fc98ad2c6370788615ba95dd
rian <70577975+rianprakasa@users.noreply.github.com>
2024-02-15 16:14:23 +0700

    Add files via upload

---
+++ my valentine/js/script1.js
@@ -0,0 +1,57 @@
+function showMessage(response) {
+    if (response === "No") {
+      const noButton = document.getElementById("no-button");
+      const container = document.querySelector(".container");
+      const maxWidth = window.innerWidth - noButton.offsetWidth;
+      const maxHeight = window.innerHeight - noButton.offsetHeight;
+  
+      // Set button position to absolute
+      noButton.style.position = "absolute";
+  
+      // Change image source to "gun.gif"
+      document.getElementsByClassName("image")[0].src = "img/angry.gif";
+  
+      // Generate random coordinates within the visible container
+      const randomX = Math.max(0, Math.floor(Math.random() * maxWidth));
+      const randomY = Math.max(0, Math.floor(Math.random() * maxHeight));
+  
+      // Apply new coordinates to the button
+      noButton.style.left = randomX + "px";
+      noButton.style.top = randomY + "px";
+  
+      // Update text content and hide name message
+      document.getElementById("question").textContent =
+        "kenapa pilih itu!";
+      document.getElementById("name").style.display = "none";
+  
+      // Optional: You can also add a timeout to reset the position after a few seconds
+    }
+  
+    if (response === "Yes") {
+      // Remove name message and no button
+      document.getElementById("name").remove();
+      document.getElementById("no-button").remove();
+      // document.getElementById("yesButton").remove();
+
+      const yesButton = document.getElementById("yesButton");
+      yesButton.textContent = "";
+      const link = document.createElement('a')
+      const teksLink = document.createTextNode('coba klik ini beb')
+      link.appendChild(teksLink)
+      link.href = "index2.html";
+      link.style.color = "white"
+      link.style.textDecoration = 'none'
+      yesButton.appendChild(link)
+      
+  
+      // Update text content, show message, and change image source to "dance.gif"
+      const yesMessage = document.getElementById("question");
+      yesMessage.textContent = "yeeyy, aku juga sayang kamu beb😘😘";
+      yesMessage.style.display = "block";
+      yesMessage.style.fontStyle = "normal";
+      document.getElementsByClassName("image")[0].src = "img/love.gif";
+  
+      // Remove yes button
+    }
+  }
+  
